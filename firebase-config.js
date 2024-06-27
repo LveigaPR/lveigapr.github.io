aa88ec1ffa682687e94a55a4d6efd08272612cfe
LveigaPR <124899567+LveigaPR@users.noreply.github.com>
2024-06-27 16:56:19 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBw7WsADyurlSffB_W0kLgSy6thrrz4rKY",
+    authDomain: "autenticacao-pagina-git.firebaseapp.com",
+    projectId: "autenticacao-pagina-git",
+    storageBucket: "autenticacao-pagina-git.appspot.com",
+    messagingSenderId: "43697102834",
+    appId: "1:43697102834:web:12b8fe5a85d47f400dd56b",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
